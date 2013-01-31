204e421c94b29c89ec96e733d4e808fadb02a3cb
jglee1 <jglee1@gmail.com>
2013-01-31 16:12:41 +0000

    new file:   .gitignore 	new file:   Gemfile 	new file:   Gemfile.lock 	new file:   Gemfile~ 	new file:   README.rdoc 	new file:   Rakefile 	new file:   app/assets/images/rails.png 	new file:   app/assets/javascripts/application.js 	new file:   app/assets/stylesheets/application.css 	new file:   app/assets/stylesheets/application.css~ 	new file:   app/controllers/application_controller.rb 	new file:   app/controllers/movies_controller.rb 	new file:   app/controllers/movies_controller.rb~ 	new file:   app/helpers/application_helper.rb 	new file:   app/mailers/.gitkeep 	new file:   app/models/.gitkeep 	new file:   app/models/movie.rb 	new file:   app/models/movie.rb~ 	new file:   app/views/layouts/application.html.erb.bak 	new file:   app/views/layouts/application.html.haml 	new file:   app/views/layouts/application.html.haml~ 	new file:   app/views/movies/edit.html.haml 	new file:   app/views/movies/index.html.haml 	new file:   app/views/movies/index.html.haml~ 	new file:   app/views/movies/new.html.haml 	new file:   app/views/movies/show.html.haml 	new file:   app/views/movies/show.html.haml~ 	new file:   config.ru 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/initializers/wrap_parameters.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   config/routes.rb~ 	new file:   db/migrate/20121209001801_create_movies.rb 	new file:   db/migrate/20121209001801_create_movies.rb~ 	new file:   db/schema.rb 	new file:   db/seeds.rb 	new file:   db/seeds.rb~ 	new file:   doc/README_FOR_APP 	new file:   lib/assets/.gitkeep 	new file:   lib/tasks/.gitkeep 	new file:   log/.gitkeep 	new file:   public/404.html 	new file:   public/422.html 	new file:   public/500.html 	new file:   public/favicon.ico 	new file:   public/index.html.bak 	new file:   public/robots.txt 	new file:   script/rails 	new file:   vendor/assets/javascripts/.gitkeep 	new file:   vendor/assets/stylesheets/.gitkeep 	new file:   vendor/plugins/.gitkeep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'c2e035d9106ab5dccbf7a343b64711c17d4085d0345f7044824a6df0205e815b26565615fc2a7f2d49e9ec25f405f295f44a090ebd89ee3abcab81942a82cd35'
